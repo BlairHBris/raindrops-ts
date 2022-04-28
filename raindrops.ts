0c1315f7648680ff3147e9387b0b67b7286bfffc
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 16:42:03 -0400

    Add solution

--- raindrops.ts
+++ raindrops.ts
@@ -1,1 +1,21 @@
-export function convert() {}
+export function convert(number: number) {
+    let string = ""
+
+    if(number % 3 == 0) {
+        string += "Pling"
+    }
+    
+    if(number % 5 == 0){
+        string += "Plang"
+    }
+
+    if(number % 7 == 0){
+        string += "Plong"
+    }
+
+    if(string.length > 0){
+        return string
+    } else {
+        return number.toString()
+    }
+}
